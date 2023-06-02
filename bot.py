9f6666b2c55255e10369cc33371a65269bb2dbb7
dmtof <dmtoof@gmail.com>
2023-06-02 14:20:40 +0300

    second commit

--- bot.py
+++ bot.py
@@ -1,37 +1,25 @@
-# import os, sys
-# activate_this = '/home/bot/python/bin/activate_this.py'
-# with open(activate_this) as f:
-#     exec(f.read(), {'__file__': activate_this})
-
-# import telebot
-# from config import TOKEN
-# from telebot import types
-
-# bot = telebot.TeleBot(TOKEN)
-
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Здравствуйте, какая у вас проблема?')
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-#     item1 = types.KeyboardButton('1....')
-#     item2 = types.KeyboardButton('2....')
-#     item3 = types.KeyboardButton('3....')
-#     item4 = types.KeyboardButton('4....')
-#     item5 = types.KeyboardButton('5....')
-
 import os, sys
 activate_this = '/home/bot/python/bin/activate_this.py'
 with open(activate_this) as f:
     exec(f.read(), {'__file__': activate_this})
 
 import telebot
+from telebot import types
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Я жив!!!')
+    markup = type.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('first')
+    item2 = types.KeyboardButton('second')
+    item3 = types.KeyboardButton('third')
+    item4 = types.KeyboardButton('fourth')
+
+    markup.add(item1, item2, item3, item4)
+
+    bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup = markup)
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
