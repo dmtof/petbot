7206a7551cb50c55680cb6a440e72ce292c545c1
dmtof <dmtoof@gmail.com>
2023-06-02 13:52:53 +0300

    test commit

--- bot.py
+++ bot.py
@@ -5,15 +5,16 @@ with open(activate_this) as f:
 
 import telebot
 from config import TOKEN
+from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Я жив!')
-
-@bot.message_handler(content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
-
-bot.polling(none_stop=True, interval=0)
+    bot.send_message(message.chat.id, 'Здравствуйте, какая у вас проблема?')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('1....')
+    item2 = types.KeyboardButton('2....')
+    item3 = types.KeyboardButton('3....')
+    item4 = types.KeyboardButton('4....')
+    item5 = types.KeyboardButton('5....')
