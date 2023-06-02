668d861888c0b942636b53b298a0abe7552eda57
dmtof <dmtoof@gmail.com>
2023-06-02 14:12:48 +0300

    test commit

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Я жив!')
+    bot.send_message(message.chat.id, 'Я жив!!!')
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
